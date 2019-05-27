3022a8f9c9cf18a9c6735d5284a819b16419a5b1
killgcd <linling07@gmail.com>
2019-05-27 15:21:12 +1000

    update

---
+++ FirefoxFQ/0.trojan翻墙.cmd
@@ -0,0 +1,7 @@
+%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
+xcopy /Y %~dp0Firefox\defaults\pref\local-settings-v2  %~dp0Firefox\defaults\pref\local-settings.js
+cls
+@echo off
+start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" https://www.bannedbook.org/bnews/fq/?utm_source=ff-trojan
+cd trojan
+start "" "trojan.exe"
