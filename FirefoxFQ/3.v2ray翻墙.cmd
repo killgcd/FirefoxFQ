73100ed68657ac5ba800fc18bcc6a12c9a0a97c1
killgcd <linling07@gmail.com>
2019-06-24 16:36:48 +1000

    update

--- FirefoxFQ/3.v2ray翻墙.cmd
+++ FirefoxFQ/3.v2ray翻墙.cmd
@@ -1,6 +1,6 @@
 %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
-xcopy /Y %~dp0Firefox\defaults\pref\local-settings-v2  %~dp0Firefox\defaults\pref\local-settings.js
+xcopy /Y %~dp0Firefox\defaults\pref\local-settings-v2new  %~dp0Firefox\defaults\pref\local-settings.js
 cls
 @echo off
 start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" https://www.bannedbook.org/bnews/fq/?utm_source=ff-v2ray
-start "" "%~dp0v2ray\v2ray.exe"
+start "" "%~dp0v2ray\v2rayN.exe"
