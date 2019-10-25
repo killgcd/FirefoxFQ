0d85c795cad65923d0bb2a0885aad9c518131ca6
killgcd <linling07@gmail.com>
2019-10-25 16:22:56 +1100

    update

--- FirefoxFQ/3.Agent3翻墙.cmd
+++ FirefoxFQ/3.Agent3翻墙.cmd
@@ -2,5 +2,22 @@
 xcopy /Y %~dp0Firefox\defaults\pref\local-settings-ss  %~dp0Firefox\defaults\pref\local-settings.js
 cls
 @echo off
+echo 是否执行IP更新？IP更新从云端更新IP配置以解决封锁问题！
+echo 按3跳过，按1选择ip1更新，若ip1不好用再按2选ip2更新。
+choice /C 123 /T 15 /D 3 /M "1.ip1更新,2.ip2更新,3.跳过"
+if errorlevel 3 goto startfq
+if errorlevel 2 goto ip2
+if errorlevel 1 goto ip1
+
+:ip2
+start /wait "" "%~dp0Agent3\ip_Update\ip_2.bat"
+goto startfq
+
+:ip1
+start /wait "" "%~dp0Agent3\ip_Update\ip_1.bat"
+goto startfq
+
+:startfq
+
 start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" https://www.bannedbook.org/bnews/fq/?utm_source=ff-SSR
 start "" "%~dp0Agent3\ShadowsocksR.exe"
