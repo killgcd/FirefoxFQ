573494177403fa309d785de64794e35011665568
killgcd <linling07@gmail.com>
2019-12-20 22:52:09 +1100

    update

--- FirefoxFQ/1.蓝灯翻墙.cmd
+++
@@ -1,9 +0,0 @@
-%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
-xcopy /Y %~dp0Firefox\defaults\pref\local-settings-lt  %~dp0Firefox\defaults\pref\local-settings.js
-cls
-@echo off
-start "" "%~dp0Lantern\lantern.exe"
-echo ÕýÔÚÆô¶¯À¶µÆ£¬ÇëÉÔºò...
-start /wait "" "%~dp0Lantern\setting.vbs"
-start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" http://www.bannedbook.org/bnews/fq/?utm_source=ffLT3
-exit
--- FirefoxFQ/7.直连翻墙.cmd
+++
@@ -1,6 +0,0 @@
-%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
-xcopy /Y %~dp0Firefox\defaults\pref\local-settings-lt  %~dp0Firefox\defaults\pref\local-settings.js
-cls
-@echo off
-start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" https://lihi.cc/PZxJ1/FirefoxFQ
-exit
