7ab711be6d2d02e24219d13108d7090e9581b5ca
killgcd <linling07@gmail.com>
2020-02-28 14:17:34 +0800

    update

--- FirefoxFQ/2.XX-Net翻墙.cmd
+++ FirefoxFQ/2.XX-Net翻墙.cmd
@@ -5,5 +5,3 @@ cls
 start "" "%~dp0XX-Net\start.bat"
 start /wait "" "%~dp0waiting.vbs"
 start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" https://www.bannedbook.org/bnews/fq/?utm_source=ff-XX-Net
-echo 由于封锁严重，因此可能需要数分钟到数小时的初始化IP扫描，方能正常运行。如果刚启动时打不开网页，请耐心等待初始化IP扫描，运行一段时间后，使用效果会更好...
-pause
