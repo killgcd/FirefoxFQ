52c69fb1790cca47903d60eaba5e3d7326d71a1f
killgcd <linling07@gmail.com>
2020-02-29 07:28:23 +0800

    update

--- FirefoxFQ/6.Daze翻墙.cmd
+++ FirefoxFQ/6.Daze翻墙.cmd
@@ -4,14 +4,15 @@ cls
 @echo off
 
 echo 是否执行IP更新？IP更新从云端更新IP配置以解决封锁问题！
-choice /C 12 /T 15 /D 2 /M "1.ip更新,2.跳过"
+echo 按2跳过，按1选择ip1更新，如果更新后还是用不了，请发邮件到kebi2014@gmail.com进行反馈！
+choice /C 12 /T 15 /D 2 /M "1、ip更新  2、跳过"
 if errorlevel 2 goto startfq
-if errorlevel 1 goto ip1
+if errorlevel 1 goto ip
 
-:ip1
+:ip
 start /wait "" "%~dp0DAZE\ip_Update\ip_1.bat"
 goto startfq
 
 :startfq
-start %~dp0DAZE\DAZETools.exe
+start %~dp0DAZE\daze.bat
 start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" https://www.bannedbook.org/bnews/fq/?utm_source=ff-DAZE
