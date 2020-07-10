d6c43e175be05095b26f3bd165bbf4db10f2231e
killgcd <linling07@gmail.com>
2020-07-10 19:47:15 +0800

    update

--- FirefoxFQ/2.XX-Net翻墙.cmd
+++
@@ -1,9 +0,0 @@
-%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
-xcopy /Y %~dp0Firefox\defaults\pref\local-settings-xx  %~dp0Firefox\defaults\pref\local-settings.js
-cls
-@echo off
-%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-CD /D "%~dp0"
-start "" "%~dp0XX-Net\start.bat"
-start /wait "" "%~dp0waiting.vbs"
-start "" "%~dp0Firefox/firefox.exe" -no-remote -profile "%~dp0Firefox/Profile" https://www.bannedbook.org/bnews/fq/?utm_source=ff-XX-Net
--- FirefoxFQ/Firefox/defaults/pref/local-settings.js
+++ FirefoxFQ/Firefox/defaults/pref/local-settings.js
@@ -1,2 +1,2 @@
-pref("general.config.filename", "mozilla-v2new.cfg");
+pref("general.config.filename", "mozilla-v2.cfg");
 pref("general.config.obscure_value", 0);
